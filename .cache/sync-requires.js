5529444e4ace9d7e13d59f49865c362b9cc997da
Moe Ismail <ismail@Moes-MacBook-Pro.local>
2019-12-31 14:20:09 +0200

    updated

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,9 +5,9 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/ismail/node-projects/gatsby-projects/Gatsby-UnityWGL-BG/.cache/dev-404-page.js"))),
-  "component---src-pages-404-tsx": hot(preferDefault(require("/Users/ismail/node-projects/gatsby-projects/Gatsby-UnityWGL-BG/src/pages/404.tsx"))),
-  "component---src-pages-index-tsx": hot(preferDefault(require("/Users/ismail/node-projects/gatsby-projects/Gatsby-UnityWGL-BG/src/pages/index.tsx"))),
-  "component---src-pages-page-2-tsx": hot(preferDefault(require("/Users/ismail/node-projects/gatsby-projects/Gatsby-UnityWGL-BG/src/pages/page-2.tsx")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/ismail/Projects/Gatsby-WEBGL-BG/.cache/dev-404-page.js"))),
+  "component---src-pages-404-tsx": hot(preferDefault(require("/Users/ismail/Projects/Gatsby-WEBGL-BG/src/pages/404.tsx"))),
+  "component---src-pages-index-tsx": hot(preferDefault(require("/Users/ismail/Projects/Gatsby-WEBGL-BG/src/pages/index.tsx"))),
+  "component---src-pages-page-2-tsx": hot(preferDefault(require("/Users/ismail/Projects/Gatsby-WEBGL-BG/src/pages/page-2.tsx")))
 }
 
---
+++ public/styles-d3b89e02f0d0da1980b5.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{"8ypT":function(n,o,p){},ixuA:function(n,o,p){}}]);
+//# sourceMappingURL=styles-d3b89e02f0d0da1980b5.js.map
