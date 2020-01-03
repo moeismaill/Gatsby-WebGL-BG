c3716d0f40b9c4507f76b6484cd647f41c3af40f
mrismail91 <mohammad.ismail9190@gmail.com>
2020-01-03 16:19:46 +0200

    add layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,7 +4,7 @@ import Helmet from 'react-helmet';
 import { StaticQuery, graphql } from 'gatsby';
 
 import '../assets/sass/business-casual.scss';
-import Footer from './Footer';
+import Footer from './footer';
 import SiteHeader from './siteheader';
 import Header from './header';
 
