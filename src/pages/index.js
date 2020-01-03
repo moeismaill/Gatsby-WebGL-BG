e7773b7571e0fdc3794618a1cc6670072539f4bb
mrismail91 <mohammad.ismail9190@gmail.com>
2020-01-03 10:06:49 +0200

    return Layout to layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
 
-import Layout from "../components/Layout"
+import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Scroller from "../components/scroller"
 import PortfolioModal from "../components/portfolio/modal"
